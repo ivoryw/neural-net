c31408a4c93f0f58adc7de7e0cd94b08c7d021f8
Will <will.ivory1@gmail.com>
2017-08-12 18:11:27 +0100

    Changed network vectors to forward lists

--- src/net.cc
+++ src/net.cc
@@ -1,17 +1,8 @@
 #include "net.hpp"
 
 namespace nn{
-Net::Net(double lr) : learning_rate(lr){}
-
 void Net::backwardProp(const autodiff::var& loss){
     loss.evaluateLeaves();
 }
 
-void Net::update(){
-    while(!param_list.empty()){
-        auto param_update = param_list.top()->grad() * learning_rate;
-        *(param_list.top()) += param_update;
-        param_list.pop();
-    }
-}
 } // namespace nn
--- src/net.hpp
+++ src/net.hpp
@@ -1,25 +1,22 @@
 #ifndef NET_H
 #define NET_H
 #include "autodiff.hpp"
-#include <stack>
+#include <forward_list>
 
 namespace nn{
 class Net{
-    public:
-    Net(double=0.01);
+public:
     void backwardProp(const autodiff::var &loss);
-    void update();
     class Parameter : public autodiff::var{
         public:
         Parameter(Net* net, const Tensor& data) 
-        : autodiff::var(data){ net->param_list.push(this); } 
-        Parameter(Net* net ,size_t x, size_t y=1, size_t z=1, size_t t=1)
-        : autodiff::var(x,y,z,t){ net->param_list.push(this); }
+        : autodiff::var(data){ net->param_list.push_front(this); } 
+        Parameter(Net* net, size_t x, size_t y=1, size_t z=1, size_t t=1)
+        : autodiff::var(x,y,z,t){ net->param_list.push_front(this); }
     };
-    private:
-    double learning_rate;
-    protected:
-    std::stack<Parameter*> param_list;
+    std::forward_list<Parameter*>& params() {return param_list;}
+protected:
+    std::forward_list<Parameter*> param_list;
 };
 } // namespace nn
 #endif // NET_H
