a948b2b599059117986190ebf7bc18661677a831
Will <will.ivory1@gmail.com>
2017-08-12 20:52:11 +0100

    Removed old hardcoded GD

--- src/net.hpp
+++ src/net.hpp
@@ -1,25 +1,22 @@
 #ifndef NET_H
 #define NET_H
 #include "autodiff.hpp"
-#include <stack>
+#include <forward_list>
 
 namespace nn{
 class Net{
     public:
-    Net(double=0.01);
     void backwardProp(const autodiff::var &loss);
-    void update();
     class Parameter : public autodiff::var{
-        public:
+    public:
         Parameter(Net* net, const Tensor& data) 
-        : autodiff::var(data){ net->param_list.push(this); } 
+        : autodiff::var(data){ net->param_list.push_front(this); } 
         Parameter(Net* net ,size_t x, size_t y=1, size_t z=1, size_t t=1)
-        : autodiff::var(x,y,z,t){ net->param_list.push(this); }
+        : autodiff::var(x,y,z,t){ net->param_list.push_front(this); }
     };
-    private:
-    double learning_rate;
+    std::forward_list<Parameter*>& params() {return param_list;}
     protected:
-    std::stack<Parameter*> param_list;
+    std::forward_list<Parameter*> param_list;
 };
 } // namespace nn
 #endif // NET_H
