d77c9e7ab18a1eb08a82f0b81175ae5790f12e55
Will <will.ivory1@gmail.com>
2017-08-19 23:10:18 +0100

    Added MSE and cross entropy

--- src/loss.cc
+++ src/loss.cc
@@ -8,4 +8,14 @@ autodiff::var mean(autodiff::var& input, autodiff::var& target){
     loss /= l_size;
     return loss;
 }
+autodiff::var mse(autodiff::var& input, autodiff::var& target){
+    auto loss = (pow(input - target, 2)).asum();
+    auto l_size = autodiff::var(nn::Tensor(loss.data.shape, input.size()));
+    loss /= l_size;
+    return loss;
 }
+autodiff::var crossEntropy(autodiff::var& input, autodiff::var& target){
+    auto loss = -1 * (input * log(target)).sum();
+    return loss;
+}
+} // namespace loss
--- src/loss.hpp
+++ src/loss.hpp
@@ -2,5 +2,7 @@
 
 namespace loss{
 autodiff::var mean(autodiff::var&, autodiff::var&);
+autodiff::var mse(autodiff::var&, autodiff::var&);
+autodiff::var crossEntropy(autodiff::var&, autodiff::var&);
 } // namepsace loss
 
