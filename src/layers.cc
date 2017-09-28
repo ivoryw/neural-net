22fe13d938841912f17c3fd24abeb90e0a2549f7
Will <will.ivory1@gmail.com>
2017-09-28 21:06:43 +0100

    Implemented Conv1d and padding, needs autodiff sub-matrix to work

--- src/layers.cc
+++ src/layers.cc
@@ -21,20 +21,38 @@ Var FullyConnected::operator()(const Var& input){
     return result;
 }
 
-//Conv1d::Conv1d(Net* net, size_t c_in, size_t c_out, size_t kernel, size_t padding, size_t stride)
-//: weight(net, c_out, c_in, kernel, kernel), bias(net, c_out),
-// out_channels(c_out), kernel(kernel), padding(padding), stride(stride){
-//    weight.randn();
-//    bias.randn();
-//}
+Conv1d::Conv1d(Net* net, size_t c_in, size_t c_out, size_t kernel, size_t padding)
+: weight(net->create_parameter(Tensor(c_out, c_in, kernel))),
+bias(net->create_parameter(Tensor(c_out))),
+out_channels(c_out), kernel(kernel), padding(padding) {
+    weight.randn();
+    bias.randn();
+}
 
-//autodiff::Var Conv1d::operator()(const autodiff::Var& input){
-//    auto r_len = floor((input.data.shape[1] + 2*padding - (kernel - 1) -1)/stride+1);
-//    autodiff::Var result(out_channels, r_len);
-//    result += conv_1d(input, weight);
-//    result += bias;
-//    return result;
-//}
+Var Conv1d::operator()(const autodiff::Var& input) {
+    auto padded_input = pad(input);
+    auto r_len = floor((padded_input.data.shape[1] + 2*padding - (kernel - 1) -1)/1);   
+    Var result(out_channels, r_len);
+    for(size_t i = 0; i < result.data.shape[2]; ++i) {
+        result.set_row(i,conv1d(weight.col(i),input));
+        result.set_row(i, result.row(i)+bias(i));
+   	}
+    result += bias;
+    return result;
+}
+
+Var Conv1d::pad(const Var& input) {
+    Var padded(input.data.shape[0] + padding*2, input.data.shape[1]);
+    for(size_t i = 0; i < padded.data.shape[0]; ++i) {
+        if(i < padding || i >= padded.data.shape[0] - padding) {
+            padded.set_row(i, nn::Tensor({{padded.data.shape[0], 1, 1}}, 0));
+        }
+        else{
+            padded.set_row(i, input.row(i - padding));
+        }
+    }
+    return padded;
+}
 
 
 //Conv2d::Conv2d(Net* net, size_t c_in, size_t c_out, size_t kernel, size_t padding, size_t stride)
--- src/layers.hpp
+++ src/layers.hpp
@@ -14,14 +14,15 @@ using autodiff::Var;
         Var operator()(const Var&);
     };
 
-//   class Conv1d{
-//   private:
-//       Net::Parameter weight, bias;
-//       const size_t out_channels, kernel, padding, stride;
-//   public:
-//       Conv1d(Net*, size_t, size_t, size_t, size_t=0, size_t=1);
-//       autodiff::Var operator()(const autodiff::Var&);
-//   };
+  class Conv1d{
+  private:
+      Var &weight, &bias;
+      const size_t out_channels, kernel, padding;
+      Var pad(const Var&);
+  public:
+      Conv1d(Net*, size_t, size_t, size_t, size_t=0);
+      Var operator()(const autodiff::Var&);
+  };
 
 //   class Conv2d{
 //   private:
