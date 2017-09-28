26567216abc11a148bd753444dea33da511f0556
Will <will.ivory1@gmail.com>
2017-09-28 20:41:53 +0100

    Comments for clarity

--- src/net.hpp
+++ src/net.hpp
@@ -6,9 +6,16 @@
 namespace nn{
 using autodiff::Var;
 
+/**
+	Net
+	An abstract neural net object using automatic differentiation
+	A forward function should be defined by the end-user within a child class
+*/
 class Net{    
     public:
+    // Backpropigation using the chain rule and the AutoDiff module
     void backward(const Var &loss);
+    // Parameter registration and creation
     Var& create_parameter(const Tensor& data) {
         parameters.push_front(Var(data));
         return parameters.front();
--- src/optim.hpp
+++ src/optim.hpp
@@ -9,6 +9,10 @@ namespace opt{
 using autodiff::Var;
 typedef std::forward_list<Var> ParameterList;
 
+/**
+    Opt
+    Abstract optimiser storing a reference to a list of autodiff::Vars
+*/
 class Opt{
 public:
     Opt(ParameterList&);
@@ -18,18 +22,28 @@ protected:
     ParameterList& parameters;
 };
 
+/**
+    GD
+    Gradient descent optimisation of a ParameterList
+*/
 class GD: public Opt{
 public:
     GD(ParameterList&, double = 0.1);
+    // One optimisation step
     void step();    
     ~GD(){};
 private:
     double l_rate;
 };
 
+/**
+    Moment
+    Gradient descent optimisation of a ParameterList with momentum
+*/
 class Moment : public Opt{
 public:
     Moment(ParameterList&, double = 0.1, double = 0.9);
+    // One optimisation step
     void step();
     ~Moment(){}
 private:
